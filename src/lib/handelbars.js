60376cc72b90f9ca4ca513821b0a5059e480c4df
ae-tafur <ae.tafur@uniandes.edu.co>
2021-03-18 07:56:29 -0500

    Initial commit

--- src/lib/handelbars.js
+++
@@ -1,9 +0,0 @@
-const JsBarcode = require('jsbarcode');
-
-const helpers = {};
-
-helpers.JsBarcode = (savedTimestamp) => {
-    return JsBarcode(".barcode").init();
-};
-
-module.exports = helpers;
