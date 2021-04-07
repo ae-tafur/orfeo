71e6d4168ef739dcfbc38bc66c153d4dafcb51a6
ae-tafur <ae.tafur@uniandes.edu.co>
2021-04-06 19:35:30 -0500

    Restart

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
- /* Importing the Main App */
-  
- const app = require('./app');
- 
- /* Starting the server */
-
- app.listen(app.get('port'));
- console.log('Server is in port', app.get('port'));
--- src/lib/helpers.js
+++
@@ -1,8 +0,0 @@
-const helpers = {};
-
-import MCDatepicker from 'mc-datepicker';
-helpers.myDatePicker = MCDatepicker.create({ 
-  el: '#example' 
-});
-
-module.exports = helpers;
